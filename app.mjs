d2407a51800ecc869f543cfc462e752703471b7c
ebenezerdon <ebenezerdonu@gmail.com>
2022-04-06 13:47:26 +0100

    do express-graphql setup

--- app.mjs
+++ app.mjs
@@ -1,6 +1,25 @@
 import express from 'express'
+import { graphqlHTTP} from 'express-graphql'
+import { buildSchema } from 'graphql'
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`)
+
+const root = {
+  hello: () => {
+    return 'Hello world!'
+  }
+}
 
 const app = express()
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
