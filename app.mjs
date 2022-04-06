150542741f9402657731c75e90e462076a87e0a7
ebenezerdon <ebenezerdonu@gmail.com>
2022-04-06 09:40:23 +0100

    do only express setup

--- app.mjs
+++ app.mjs
@@ -1,33 +1,13 @@
-import { ApolloServer } from 'apollo-server-express'
 import express from 'express'
-import { GraphQLObjectType, GraphQLSchema } from 'graphql'
-import { noteMutation } from './schema/mutations.mjs'
-import noteQueries from './schema/queries.mjs'
-
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'Query',
-    fields: {
-      ...noteQueries,
-    },
-  }),
-  mutation: new GraphQLObjectType({
-    name: 'Mutation',
-    fields: {
-      ...noteMutation,
-    },
-  }),
-})
 
 const app = express()
-const server = new ApolloServer({ schema })
 
-await server.start()
-
-server.applyMiddleware({ app })
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-  console.log(`🚀 Find me on http://localhost:${port + server.graphqlPath}`)
+  console.log(`🚀 Find me on http://localhost:${port}`)
 })
